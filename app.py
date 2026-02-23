a8fe88b2bb435518598bbc22f4ccefffb922d4a5
kannang-ui <colcolkannang@gmail.com>
2026-02-23 21:08:44 +0100

    Mise a jour du message d'acceuil

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Félicitations !</h1><p>Ton conteneur Docker fonctionne parfaitement.</p>'
+    return '<h1>Félicitations !</h1><p>Bienvenue sur ma version 2.0!.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
